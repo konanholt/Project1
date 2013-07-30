824fffbd82dc932dad01a6c97bfd8a8247e7a2f6
Konan Palmer <konan19@mac.com>
2013-07-29 21:13:29 -0600

    latest changes

--- Project8/list_dirs.c
+++ Project8/list_dirs.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 #include <dirent.h>
-
-int main() {
+void searchCurrentDir() {
 	DIR *directory = opendir("/Users/Konan/Desktop");
 	struct dirent *directoryent;
 	if (directory) {
-		while ((directoryent = readdir(directory)))	{
-			printf("%s\n", directoryent->d_name);
-		}
-	} else {
-		fprintf(stderr, "error: cannot access directory\n");
-	}
-
+                while ((directoryent = readdir(directory)))     {
+                        printf("%s\n", directoryent->d_name);
+                }
+        } else {
+                fprintf(stderr, "error: cannot access directory\n");
+        }
+}
+int main() {
+	searchCurrentDir();
 	return 0;
 }
 
